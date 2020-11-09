da30b8796363afe2c658bd31b4c729f676303d8b
root <root@EDGARHP.localdomain>
2020-11-09 19:19:38 +0300

    fixed warning message during import

--- src/setup_py/mpinv/__init__.py
+++ src/setup_py/mpinv/__init__.py
@@ -1,5 +1,5 @@
 from .mpinv_wrapper import *
 from .mpmath_wrapper import *
 
-print("WARNING: current version works only with 50 mpmath digits. Include \"mpmath.mp.dps = 50\" to your code")
+print("WARNING: current mpinv version works only with 50 mpmath digits. Include \"mpmath.mp.dps = 50\" to your code")
 import mpmath as mp; mp.mp.dps = 50
