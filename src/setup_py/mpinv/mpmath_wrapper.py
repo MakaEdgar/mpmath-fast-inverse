d0f050b794982ebdd55cbcf6b9378036e112be3a
Edgar Makarov <makaed@yandex.ru>
2020-11-09 19:38:49 +0300

    added mpmat files loading

--- src/setup_py/mpinv/mpmath_wrapper.py
+++ src/setup_py/mpinv/mpmath_wrapper.py
@@ -12,4 +12,17 @@ def slow_mp_matrix_inverse_with_logdet(m):
     return (logdet, m_inv)
 
 
+def mp_matrix_to_mpmatfile(m, outfile="matrix.mpmat"):
+    with open(outfile, "w") as f:
+        f.write(str(m.rows) + " " + str(m.cols) + "\n")
+        for row in m.tolist():
+            for m_ij in row:
+                f.write(str(m_ij) + " ")
+            f.write("\n")
+
+def mp_matrix_from_mpmatfile(infile="matrix.mpmat"):
+    with open(infile, "r") as f:
+        file = [x.strip().split() for x in f.read().split("\n") if x.strip() != ""]
+    matrix_list = [list(map(mp.mpf, x)) for x in file[1:]]
+    return mp.matrix(matrix_list)
 
